b10aad41def8719e4b4d661f2cb98ac4355a8d0a
Виктория Земцева <jlgfmhldf@gmail.com>
2017-02-01 00:06:48 +0300

    save

--- script.js
+++ script.js
@@ -2,119 +2,84 @@ window.onload = function () {
 	var
 		api = {
 			ajax: function ( path, data ) {
-//				const xhr = new XMLHttpRequest()
-
 				const headers = new Headers({
 					'Content-Type': 'application/json; charset=UTF8',
 					'X-Accept': 'application/json'
 				})
 
-				const promise =  fetch(`https://getpocket.com/v3/${path}`, {
+				const status = response => {
+					if (response.status >= 200 && response.status < 300) {
+						return Promise.resolve(response.json())
+					} else {
+						return Promise.reject(new Error(response.statusText))
+					}
+				}
+
+				return fetch(`https://getpocket.com/v3/${path}`, {
 					method: 'POST',
 					body: JSON.stringify(data),
 					headers
-				})
-
-				console.log(promise)
-
-				return promise
-
-//				xhr.open('POST', `https://getpocket.com/v3/${path}`, true);
-//				xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF8');
-//				xhr.setRequestHeader('X-Accept', 'application/json');
-//				xhr.send(JSON.stringify(data));
-//
-//				xhr.onreadystatechange = function() {
-//
-//					console.log(xhr)
-//
-//					if (xhr.readyState === 4) {
-//						if ( xhr.status === 200) {
-//
-//							load(JSON.parse(xhr.response))
-//
-//						} else {
-//							console.log("error " + this.status);
-//
-//							error(xhr.statusText)
-//
-//							return new Error();
-//						}
-//					}
-//				}
+				}).then(status)
 			}
 		},
 		w = window,
-		isGetPocket = w.location.href.indexOf('getpocket.com') != -1
+		isGetPocket = w.location.href.indexOf('getpocket.com') != -1,
+		isSavePage = w.location.href.indexOf('learn.javascript.ru/attributes-and-custom-properties') != -1
 
 	localStorage.consumer_key = '56284-79593f636813881ec120103b';
 	localStorage.redirect_uri = w.location.href
 
-	function status(response) {
-		if (response.status >= 200 && response.status < 300) {
-			return Promise.resolve(response)
-		} else {
-			return Promise.reject(new Error(response.statusText))
-		}
+
+	const request = () =>
+		api.ajax('oauth/request', {
+			consumer_key: localStorage.consumer_key,
+			redirect_uri: localStorage.redirect_uri
+		})
+
+
+	const callbackRequest = res => {
+		window.location.href = `https://getpocket.com/auth/authorize?request_token=${localStorage.request_token}&redirect_uri=${localStorage.redirect_uri}`
 	}
 
+	 const authorize = () =>
+			 api.ajax('oauth/authorize', {
+				 consumer_key: localStorage.consumer_key,
+				 code: localStorage.request_token
+			 })
 
-//	const request = () => {
-//		api.ajax('oauth/request', {
-//			consumer_key: localStorage.consumer_key,
-//			redirect_uri: localStorage.redirect_uri
-//		}, {
-//			load: res => {
-//				localStorage.request_token = res.code;
-//
-//				window.location.href = `https://getpocket.com/auth/authorize?request_token=${localStorage.request_token}&redirect_uri=${localStorage.redirect_uri}`
-//			}
-//		});
-//	}
+	 const add = () => api.ajax('add', {
+		 title: 'Тест',
+		 url: `http://learn.javascript.ru/attributes-and-custom-properties`,
+		 consumer_key: localStorage.consumer_key,
+		 access_token: localStorage.access_token,
+		 time: Date.now(),
+	 })
 
-//	!isGetPocket && api.ajax('oauth/authorize', {
-//		consumer_key: localStorage.consumer_key,
-//		code: localStorage.request_token
-//	},{
-//		error: err => {
-//			request()
-//		},
-//		load: xhr => {
-//			console.log('user is authorized')
-//		}
-//	});
-
-	const headers = new Headers({
-		'Content-Type': 'application/json; charset=UTF8',
-		'X-Accept': 'application/json'
-	})
-//
-//	!isGetPocket && fetch(`https://getpocket.com/v3/oauth/authorize`, {
-//		method: 'POST',
-//		body: JSON.stringify({
-//			consumer_key: localStorage.consumer_key,
-//			code: localStorage.request_token
-//		}),
-//		headers
-//	}).then(
-//		responce => {
-//			console.log('success')
-//		},
-//		error => {
-//			console.log('error')
-//		})
-
-	!isGetPocket && api.ajax('oauth/authorize', {
-		consumer_key: localStorage.consumer_key,
-		code: localStorage.request_token
-	})
-		.then(status)
+	!isGetPocket && add()
 		.then(
-			responce => console.log('success'),
-			error => console.log('error')
+			response => {
+				console.log('page added')
+			},
+			error => {
+				authorize()
+					.then(
+						response => {
+							localStorage.isAuthorized = 'true'
+							return Promise.reject()
+						},
+						request()
+							.then(
+								response => {
+									localStorage.isRequested = 'true';
+									localStorage.request_token = response.code;
+									callbackRequest(response)
+								})
+					)
+
+			}
 		)
 
-	console.log('pl tst')
+	console.warn('pocket-like test')
 
 }
 
@@ -125,3 +90,4 @@ window.onload = function () {
 // Как спрятать consumer_key
 // поместить некоторые данные в chrome.storage
 // https://learn.javascript.ru/fetch
+// лучше открывать авторизацию в новом окне
