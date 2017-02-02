f1c74bdd2e25d13cab513e573ff45dde7d360014
Виктория Земцева <jlgfmhldf@gmail.com>
2017-02-02 08:40:30 +0300

    add pocket-like-api

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,37 +3,19 @@ import PocketLike from './pocket-like-api'
 window.onload = function () {
 	var w = window,
 		isPocket = w.location.href.indexOf('getpocket.com') != -1;
-
-	const consumerKey = '56284-79593f636813881ec120103b'
 	const redirectURI = window.location.href
 
-	const request = () =>
-		ajax('oauth/request', {
-			consumer_key: consumerKey,
-			redirect_uri: redirectURI,
-		})
+	const pl = new PocketLike('56284-79593f636813881ec120103b')
 
-	const authorize = (code) =>
-		 ajax('oauth/authorize', {
-			 consumer_key: consumerKey,
-			 code,
-		 })
-
-	const add = () => ajax('add', {
-		title: 'Тест',
-		url: w.location.href,
-		consumer_key: consumerKey,
-		access_token: localStorage.pl_accessToken,
-	})
-
-	!isPocket && add()
+	!isPocket && pl.add(localStorage.pl_accessToken)
 		.then(
-			response => {
-				console.log('page added')
+			() => {
+				console.info('page added!')
 			},
-			error => {
+			() => {
 				if (!localStorage.pl_requestToken) {
-					return request()
+					return pl
+						.request(redirectURI)
 						.then(({ code }) => {
 
 							localStorage.pl_requestToken = code
@@ -46,9 +28,9 @@ window.onload = function () {
 				}
 
 				if (!localStorage.pl_accessToken) {
-
-					return authorize(localStorage.pl_requestToken)
-						.then(({ access_token, username })=> {
+					return pl
+						.authorize(localStorage.pl_requestToken)
+						.then(({ access_token, username }) => {
 							localStorage.pl_accessToken = access_token
 							localStorage.pl_username = username
 						})
